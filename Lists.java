92d13787d8fb526d389c293101ec026550b7d7db
Richardalmeida21 <richard.c.dealmeida@gmail.com>
2025-02-05 21:18:48 -0300

    subindo projeto

---
+++ Lists.java
@@ -0,0 +1,8 @@
+public class Lists {
+
+    public static void main(String[] args) {
+        String curso1 = "Java: tire o proveito dos novos recursos da linguagem!";
+        String curso2 = "Apache Camel";
+        String curso3 = "Certificação Java SE programmer";
+    }
+}
