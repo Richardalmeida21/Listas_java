d2111a3d032364f854ba4509929dc94c3791725f
Richardalmeida21 <richard.c.dealmeida@gmail.com>
2025-02-05 21:49:13 -0300

    add arraylist

--- Lists.java
+++ Lists.java
@@ -1,8 +1,17 @@
+import java.util.ArrayList;
+
 public class Lists {
 
     public static void main(String[] args) {
         String curso1 = "Java: tire o proveito dos novos recursos da linguagem!";
         String curso2 = "Apache Camel";
         String curso3 = "Certificação Java SE programmer";
+
+        ArrayList<String> cursos = new ArrayList<>();
+        cursos.add(curso1);
+        cursos.add(curso2);
+        cursos.add(curso3);
+
+        System.out.println(cursos);
     }
 }
